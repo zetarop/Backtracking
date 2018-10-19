95e77611c3b712d52343f1fe3662576a38a359f0
zetarop <arkupa199888@gmail.com>
2018-10-19 10:45:02 +0530

    Update print_all_combination.cpp

--- print_all_combination.cpp
+++ print_all_combination.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+#define ll long long
+using namespace std;
 
+void Combination(ve<int> temp, int start, int n, int k)
+{
+    if(temp.size() == k)
+    {
+    	for(auto a:temp)
+    		cout<<a<<" ";
+    	cout<<endl;
+    }
+    else
+    {
+        for(int i=start+1;i<=n;i++)
+        {
+            ve<int> temp1 = temp;
+            temp1.pb(i);
+            Combination(temp1, i, n, k);
+        }
+    }
+}
+
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	ve<int> temp;
+	Combination(temp, 0, n, k);
+	return 0;
+}
